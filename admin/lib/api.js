151651dd64dd4c3b4e5fa60cc6648556df6a26b5
Priyansh Tyagi <tpriyansh973@gmail.com>
2026-04-05 23:50:18 +0530

    *

--- admin/lib/api.js
+++ admin/lib/api.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:5000/api";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
 
 async function request(path, options = {}) {
   const response = await fetch(`${API_URL}${path}`, {
